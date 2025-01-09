32614476a6726241f2aacb6a6c923e495d0f8b56
Rob Greene <robgreene@gmail.com>
2025-01-09 11:43:02 -0600

    Now adding an opcode grid to the doc pages.

--- src/main/java/a2geek/asm/site/GenerateCpuDocumentation.java
+++ src/main/java/a2geek/asm/site/GenerateCpuDocumentation.java
@@ -1,10 +1,14 @@
 package a2geek.asm.site;
 
+import a2geek.asm.definition.AddressMode;
 import a2geek.asm.definition.CpuDefinition;
+import a2geek.asm.definition.Operation;
+import a2geek.asm.definition.OperationAddressing;
 import a2geek.asm.service.AssemblerException;
 import a2geek.asm.service.AssemblerService;
 import a2geek.asm.service.DefinitionService;
 import a2geek.asm.service.DefinitionService.ValidationType;
+import a2geek.asm.service.ExpressionService;
 import io.pebbletemplates.pebble.PebbleEngine;
 import io.pebbletemplates.pebble.attributes.AttributeResolver;
 import io.pebbletemplates.pebble.error.LoaderException;
@@ -66,12 +70,40 @@ public class GenerateCpuDocumentation {
 
 		Map<String,Object> context = new HashMap<>();
 		context.put("cpu", cpu);
+		context.put("grid", buildGrid(cpu));
 
 		StringWriter sw = new StringWriter();
 		template.evaluate(sw, context);
 
 		Path path = Path.of(directory, String.format("%s.md", cpu.getName()));
 		Files.writeString(path, sw.toString());
+	}
+
+	private record GridDetail(Operation op, OperationAddressing opAddr) {}
+	private static GridDetail[] buildGrid(CpuDefinition cpu) {
+		GridDetail[] ops = new GridDetail[256];
+		for (var op : cpu.getOperations()) {
+			// NOTE: Doesn't do justice to CPUs like SWEET16 which embed a register in the opcode itself
+			for (var opAddr : op.getAddressingModes()) {
+                try {
+                    var value = ExpressionService.evaluate(opAddr.getOpcode());
+					if (value instanceof Number number) {
+						int i = number.intValue();
+						if (i < 0 || i > 255) {
+							System.err.printf("CPU: %s - opcode out of range (%02x)\n", cpu.getName(), i);
+						} else if (ops[i] != null) {
+							System.err.printf("CPU: %s - opcode value already used (%02x)\n", cpu.getName(), i);
+						} else {
+							ops[i] = new GridDetail(op, opAddr);
+						}
+					}
+                } catch (AssemblerException e) {
+                    System.err.printf("CPU: %s - unable to evaluate opcode value ('%s') - %s\n",
+							cpu.getName(), opAddr.getOpcode(), e.getMessage());
+                }
+			}
+		}
+		return ops;
 	}
 
 	private static void generateDirective() throws IOException {
@@ -140,7 +172,8 @@ public class GenerateCpuDocumentation {
 		@Override
 		public Map<String, Filter> getFilters() {
 			return Map.of(
-				"fixText", new FixTextFilter()
+				"fixText", new FixTextFilter(),
+				"hexDigit", new HexDigitFilter()
 			);
 		}
 
@@ -211,6 +244,29 @@ public class GenerateCpuDocumentation {
 		@Override
 		public List<String> getArgumentNames() {
 			return List.of(NEWLINES_ARG);
+		}
+	}
+
+	/** Format a number as a single hex digit. */
+	private static class HexDigitFilter implements Filter {
+		@Override
+		public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
+			if (input instanceof Number number) {
+				return String.format("%x", (number.intValue() & 0xf));
+			} else if (input instanceof String string) {
+				try {
+					int num = Integer.parseInt(string, 10);
+					return String.format("%x", (num & 0xf));
+				} catch (NumberFormatException e) {
+					return string;	// not a number, just pass it through
+				}
+			}
+			return input;
+		}
+
+		@Override
+		public List<String> getArgumentNames() {
+			return List.of();
 		}
 	}
 }
